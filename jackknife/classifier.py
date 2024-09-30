d68a2d7670cc650472bf15a3456da3525ec88426
Stephen Pond <stephen.pond3479@gmail.com>
2024-09-30 05:06:22 -0400

    Use DTW lower bound and correction factors

--- jackknife/classifier.py
+++ jackknife/classifier.py
@@ -13,12 +13,41 @@ class Classifier:
         self.pipe_conn = pipe_conn
         self.gesture_templates = self.get_all_templates()
 
-        # Resample templates and convert to gpdvs
+        """
+        Resample templates, convert to gpdvs, envelop for lower bound and
+        extract features for correction factors
+        """
         for gest_type in self.gesture_templates:
             for template in gest_type:
-                resampled_points = d_u.resample(template.points)
-                template.gpdvs = d_u.to_gpdvs(resampled_points)
+                template.points = d_u.resample(template.points)
+                template.gpdvs = d_u.to_gpdvs(template.points)
+                template.envelop()
+                template.extract_features()
 
+    def get_all_templates(self):
+        templates = []
+
+        for gesture_type in d_u.GESTURE_TYPES.values():
+            dir = f"{d_u.PARENT_DIR}{gesture_type}\\"
+            curr_gest_templates = []
+
+            for template_num in range(d_u.TEMPLATES_PER_GESTURE):
+                curr_template = data.Template(name=gesture_type)
+
+                template_path = f"{dir}t{template_num}.csv"
+
+                with open(template_path, "r") as template_file:
+                    temp_file_reader = csv.reader(template_file)
+                    for line in temp_file_reader:
+                        curr_template.add_point([float(val) for val in line])
+
+                    template_file.close()
+
+                curr_gest_templates.append(curr_template)  
+            templates.append(curr_gest_templates)
+        
+        return templates   
+    
     def classify(self):
         while True:
             # Request new points from data manager
@@ -44,23 +73,58 @@ class Classifier:
                     for addtl_points in range(self.ADDITIONAL_POINTS):
                         query.add_point(recvd_points.get())
                     
-                    # Resample query points and convert to gpdvs
-                    resampled_points = d_u.resample(query.points)
-                    query.gpdvs = d_u.to_gpdvs(resampled_points)
+                    """
+                    Resample query points, convert to gpdvs and extract
+                    features for correction factors
+                    """
+                    query.points = d_u.resample(query.points)
+                    query.gpdvs = d_u.to_gpdvs(query.points)
+                    query.extract_features()
 
                     for i in range(1):    
                     #for i in range(d_u.TEMPLATES_PER_GESTURE):
                         for j in range(d_u.NUM_GESTURES):
                             template = self.gesture_templates[j][i]
                             gesture_name = template.name
+
+                            lower_bound = self.lower_bound(template, query)
+                            # Skip dtw check if current best score is lower than
+                            # best possible score for query and current template
+                            if query.best_score < lower_bound:
+                                continue
+                  
                             score = self.dtw(template.gpdvs, query.gpdvs)
                             
+                            correction_factors = self.correction_factors(template,
+                                                                    query)
+                            num_correction_factors = len(correction_factors)
+                            for i in range(num_correction_factors):
+                                score *= correction_factors[i]
+
                             # Using argmin of dtw for best gesture match
                             if score < query.best_score:
                                 query.best_score = score
                                 query.best_gest_name = gesture_name
                     
                 print(query.best_gest_name)
+
+
+    def lower_bound(self, template, query):
+        num_vecs = len(template.gpdvs)
+        components_per_vec = len(template.gpdvs[0])
+        lb = 0
+
+        for i in range(num_vecs):
+            lb_inner_product = 0
+            for j in range(components_per_vec):
+                if query.gpdvs[i][j] >= 0:
+                    lb_inner_product += template.upper[i][j] * query.gpdvs[i][j]
+                else:
+                    lb_inner_product += template.lower[i][j] * query.gpdvs[i][j]
+
+            lb += 1 - min(1, max(-1, lb_inner_product))
+
+        return lb
     
     def dtw(self, template_gpdvs, query_gpdvs):
         n = len(template_gpdvs) + 1
@@ -85,26 +149,18 @@ class Classifier:
     def local_cost(self, template_gpdv, query_gpdv):
         return 1 - np.inner(template_gpdv, query_gpdv)
     
-    def get_all_templates(self):
-        templates = []
+    def correction_factors(self, template, query):
+        num_feature_vecs = len(template.features)
+        correction_factors = []
 
-        for gesture_type in d_u.GESTURE_TYPES.values():
-            dir = f"{d_u.PARENT_DIR}{gesture_type}\\"
-            curr_gest_templates = []
-
-            for template_num in range(d_u.TEMPLATES_PER_GESTURE):
-                curr_template = data.Template(name=gesture_type)
-
-                template_path = f"{dir}t{template_num}.csv"
-
-                with open(template_path, "r") as template_file:
-                    temp_file_reader = csv.reader(template_file)
-                    for line in temp_file_reader:
-                        curr_template.add_point([float(val) for val in line])
-
-                    template_file.close()
-                
-                curr_gest_templates.append(curr_template)  
-            templates.append(curr_gest_templates)
+        for i in range(num_feature_vecs):
+            factor = 1
+            feature_inner_product = np.inner(template.features[i], 
+                                             query.features[i])
+            # Handles division by 0
+            if feature_inner_product != 0:
+                factor = 1 / feature_inner_product
         
-        return templates
+            correction_factors.append(factor)
+
+        return correction_factors
--- jackknife/data.py
+++ jackknife/data.py
@@ -4,24 +4,76 @@ from collections import deque
 from . import data_utils as d_u
 
 
-class Query:
+class Gesture:
     def __init__(self):
         self.points = []
         self.gpdvs = []
+        self.features = []
         self.best_score = np.inf
-        self.best_gest_name = "h"
+        self.best_gest_name = ""
 
     def add_point(self, point):
         self.points.append(point)
 
+    def extract_features(self):
+        num_vecs = len(self.gpdvs)
+        components_per_vec = len(self.gpdvs[0])
+        # Per component absolute distance traveled in gesture path
+        abs_dist_vec = np.full(components_per_vec, 0)
+        bb_max = np.copy(self.points[0])
+        bb_min = np.copy(self.points[0])
 
-class Template(Query):
+        for i in range(num_vecs):
+            for j in range(components_per_vec):
+                abs_dist_vec[j] += np.abs(self.gpdvs[i][j])
+
+                # Using i + 1 for bb vectors since points will always have one
+                #  more than gpdvs
+                bb_max[j] = max(bb_max[j], self.points[i + 1][j])
+                bb_min[j] = min(bb_min[j], self.points[i + 1][j])
+
+        abs_dist_vec_norm = np.linalg.norm(abs_dist_vec)
+        # Handles division by 0
+        if abs_dist_vec_norm != 0:
+            # Normalizing to give a relative measure of per component abs dist
+            abs_dist_vec /= abs_dist_vec_norm
+
+        bb = bb_max - bb_min
+
+        self.features.append(abs_dist_vec)
+        self.features.append(bb)
+
+
+class Query(Gesture):
+    def __init__(self):
+        super().__init__()
+
+
+class Template(Gesture):
     def __init__(self, name=""):
         super().__init__()
         self.name = name
-        self.upper_band = []
-        self.lower_band = []
-    
+        # Upper and lower bands for lower bounding dtw
+        self.upper = []
+        self.lower = []
+
+    def envelop(self):
+        num_vecs = len(self.gpdvs)
+        components_per_vec = len(self.gpdvs[0])
+
+        for i in range(num_vecs):
+            maximum = np.full(components_per_vec, -np.inf)
+            minimum = np.full(components_per_vec, np.inf)
+
+            for j in range(max(0, i - d_u.R), min(i + d_u.R + 1, num_vecs)):
+                for k in range(components_per_vec):
+                    maximum[k] = max(maximum[k], self.gpdvs[j][k])
+                    minimum[k] = min(minimum[k], self.gpdvs[j][k])
+
+            self.upper.append(maximum)
+            self.lower.append(minimum)
+
+    # For logging new templates (different from add_point)
     def record_point(self, point):
         if len(self.points) == 0 or point is not self.points[-1]:
             self.add_point(point)
